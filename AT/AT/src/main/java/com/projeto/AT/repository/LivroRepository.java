cd8f80e09907645a17137ad2e22861678ff9959b
luizeduardoardovino <131207521+luizeduardoardovino@users.noreply.github.com>
2024-09-23 09:16:15 -0300

    Delete AT/AT directory

--- AT/AT/src/main/java/com/projeto/AT/repository/LivroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projeto.AT.repository;
-
-import com.projeto.AT.model.Livro;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LivroRepository extends JpaRepository<Livro, Long> {
-}
